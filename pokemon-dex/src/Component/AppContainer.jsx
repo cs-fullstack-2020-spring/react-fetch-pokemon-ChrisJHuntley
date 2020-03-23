4493ec9c592f806599cdf4cb1540413dfd57028f
autumn-ragland <raglanda998@gmail.com>
2020-03-23 16:32:50 -0500

    graded

--- pokemon-dex/src/Component/AppContainer.jsx
+++ pokemon-dex/src/Component/AppContainer.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import Pokedexlist from "./Pokedexlist";
- 
+
 
 class AppContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-pokemon:[]
-
+            pokemon: []
         }
     }
     componentDidMount() {
@@ -19,33 +18,36 @@ pokemon:[]
 
         const Response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=900')
         const json = await Response.json();
-        this.setState({ datalist: json })
+        // console.log(json) // printing your json variable to the console is a useful check bc json is an object - not an array
+        this.setState({ datalist: json }) // looks like you copied this from the IC/lecture - your property of state is pokemon not datalist
 
 
     }
     // Method render this component
     render() {
         console.log(this.state);
+        // you should be passing something to the child element pokedex if you want it to render something from your map
         return (
             <div className='grid-container'>
                 {this.state.pokemon.map((the) => {
 
                     return (
                         <div>
-                    <p>{`PokemonName: ${the.name}`}</p>
-                    <Pokedexlist/>
-                    </div>
+                            <p>{`PokemonName: ${the.name}`}</p>
+                            <Pokedexlist />
+                        </div>
 
-                        
-                        )})}
-                </div>
+
+                    )
+                })}
+            </div>
 
 
         )
-            
-            
-            
-            
+
+
+
+
     }
 }
 export default AppContainer
--- pokemon-dex/src/Component/Pokedexlist.jsx
+++ pokemon-dex/src/Component/Pokedexlist.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 
-class Pokedexlist extends Component{
-    constructor(props){
+class Pokedexlist extends Component {
+    constructor(props) {
         super(props)
-        this.state={}
+        this.state = {}
     }
-    render(){
-        return(
-           
+    render() {
+        return (
+
             <div>
-<img src="https://pokeapi.co/api/v2/pokemon/1/" alt=""/>
+                <img src="https://pokeapi.co/api/v2/pokemon/1/" alt="" />
             </div>
         )
     }
